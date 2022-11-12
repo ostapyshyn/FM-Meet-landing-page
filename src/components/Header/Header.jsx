84604dcd11449956bf784830d986a64d2bea84bb
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2022-11-12 23:55:55 +0200

    Add tablet styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import people_tab from '../../assets/tablet/image-hero.png';
 
 const Header = () => {
   return (
     <section className={styles.header}>
       <Logo />
+      <img src={people_tab} alt="people" className={styles.image} />
       <h1>Group Chat for Everyone</h1>
       <p>
         Meet makes it easy to connect with others face-to-face virtually and collaborate across any
